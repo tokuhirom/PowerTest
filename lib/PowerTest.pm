6ad90fff26d3c814f3d98076534ab43820d97003
tokuhirom <tokuhirom@gmail.com>
2013-09-12 11:49:34 +0900

    refactoring

--- lib/PowerTest.pm
+++ lib/PowerTest.pm
@@ -91,31 +91,38 @@ our %FH_CACHE;
 our $BASE_DIR = Cwd::getcwd();
 our %FILECACHE;
 
+sub inspect_line {
+    my $level = shift;
+
+    my ($package, $filename, $line_no) = caller($level+1);
+    my $line = sub {
+        undef $filename if $filename eq '-e';
+        if (defined $filename) {
+            $filename = File::Spec->rel2abs($filename, $BASE_DIR);
+            my $file = $FILECACHE{$filename} ||= [
+                do {
+                    # Do not die if we can't open the file
+                    open my $fh, '<', $filename
+                        or return '';
+                    <$fh>
+                }
+            ];
+            my $line = $file->[ $line_no - 1 ];
+            $line =~ s{^\s+|\s+$}{}g;
+            $line;
+        } else {
+            "";
+        }
+    }->();
+    return ($package, $filename, $line_no, $line);
+}
+
 sub ok(&) {
     my $code = shift;
 
     # TODO: support method call
 
-   my ($package, $filename, $line_no) = caller(0);
-   my $line = sub {
-       undef $filename if $filename eq '-e';
-       if (defined $filename) {
-           $filename = File::Spec->rel2abs($filename, $BASE_DIR);
-           my $file = $FILECACHE{$filename} ||= [
-               do {
-                   # Do not die if we can't open the file
-                   open my $fh, '<', $filename
-                       or return '';
-                   <$fh>
-               }
-           ];
-           my $line = $file->[ $line_no - 1 ];
-           $line =~ s{^\s+|\s+$}{}g;
-           $line;
-       } else {
-           "";
-       }
-   }->();
+    my ($package, $filename, $line_no, $line) = inspect_line(0);
 
     local $@;
     my ($retval, $err, $tap_results, $op_stack)
